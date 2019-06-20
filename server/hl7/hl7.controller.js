eb4983162b6608f3529d0d481d7593dfe7b4f6fc
Perry Ogwuche <perryogwuche1@gmail.com>
2019-06-20 15:17:43 -0400

    moved sample hl7 file to data folder

--- server/hl7/hl7.controller.js
+++ server/hl7/hl7.controller.js
@@ -1,25 +1,36 @@
 const multer = require('multer');
-const Hl7 = require('./hl7.model');
+const HL7 = require('./hl7.model');
+const APIError = require('../helpers/APIError');
+const httpStatus = require('http-status');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => { cb(null, 'data/uploads'); },
+  destination: (req, file, cb) => { cb(null, 'data/hl7-uploads'); },
   filename: (req, file, cb) => (cb(null, `${file.originalname}-${Date.now()}`))
 });
 
 const upload = multer({ storage });
 
+/**
+ * Creates a new Mongoose object for the rile to save in the DB
+ * We do this mainly to avoid the (new-cap) rule error from ES6
+ */
+function newHl7File(fileName) {
+  return new HL7({ fileName });
+}
+
+/**
+ * Utilizing Multer, this function receives a file as a request, and saves the generated file name
+ * to the DB.
+ */
 function uploadFile(req, res) {
+  const hl7File = newHl7File(req.file.filename);
 
-  const hl7 = Hl7({
-    fileName: req.file.filename
-  });
-
-  hl7.save()
+  hl7File.save()
     .catch(() => {
-      res.send('I failed');
+      const err = new APIError(`Error: Failed to save file ${req.file.filename}`, httpStatus.BAD_REQUEST);
+      res.status(err.status).json(err.message);
     });
-
-  return res.send('Successful!');
+  return res.status(httpStatus.CREATED).json(hl7File);
 }
 
 module.exports = { uploadFile, upload };
