359c5a89845d5623d1b1fffaebd946c07555a427
Perry Ogwuche <perryogwuche1@gmail.com>
2019-06-24 13:43:06 -0400

    modified test file

--- server/hl7/hl7.test.js
+++ server/hl7/hl7.test.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const request = require('supertest-as-promised');
 const httpStatus = require('http-status');
 const chai = require('chai'); // eslint-disable-line import/newline-after-import
-const expect = chai.expect;
 const app = require('../../index');
 
 chai.config.includeStack = true;
