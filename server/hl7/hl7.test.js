50169ba6b9d39709ca97fb30b4b69f8426bbdf6c
jamesz <thezhubo1@gmail.com>
2019-07-10 11:32:59 -0400

    changing tests

--- server/hl7/hl7.test.js
+++ server/hl7/hl7.test.js
@@ -18,7 +18,6 @@ after((done) => {
 });
 
 describe('## File Upload', () => {
-
   describe('# POST /api/hl7/upload', () => {
     it('should upload a file to /data/hl7-uploads', (done) => {
       request(app)
@@ -35,7 +34,7 @@ describe('## File Upload', () => {
       request(app)
         .post('/api/hl7/upload')
         .attach('hl7-message', 'data/hl7-sample/test.json')
-        .expect(httpStatus.INTERNAL_SERVER_ERROR) // TODO: You shouldn't be testing for a sever error here, test for 400
+        .expect(httpStatus.BAD_REQUEST)
         .then(() => {
           done();
         })
