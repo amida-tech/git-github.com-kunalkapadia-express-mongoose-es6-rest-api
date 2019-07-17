ae22ede67b281854b60ac013d5b599f03ae0e621
Perry Ogwuche <perryogwuche1@gmail.com>
2019-07-17 17:45:58 -0400

    added module to parse hl7 message and moved test data files into new directory

--- server/hl7/hl7.test.js
+++ server/hl7/hl7.test.js
@@ -58,7 +58,7 @@ describe('## File Upload', () => {
     it('should return unauthorized without a token', (done) => {
       request(app)
         .post('/api/hl7/upload')
-        .attach('hl7-file', 'data/hl7-sample/test.txt')
+        .attach('hl7-file', 'server/tests/data/test.txt')
         .expect(httpStatus.UNAUTHORIZED)
         .then(() => {
           done();
@@ -69,7 +69,7 @@ describe('## File Upload', () => {
       request(app)
         .post('/api/hl7/upload')
         .set('Authorization', `Bearer ${userToken}`)
-        .attach('hl7-file', 'data/hl7-sample/test.json')
+        .attach('hl7-file', 'server/tests/data/test.json')
         .expect(httpStatus.BAD_REQUEST)
         .then(() => {
           done();
